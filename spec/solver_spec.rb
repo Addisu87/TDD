fa9ba7584809c7eff0be074445f06f6f65515e92
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-09-01 19:44:03 +0300

    Create test for negative numbers

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,18 +2,21 @@ require_relative '../solver.rb'
 
 describe Solver do 
   describe 'Show testing factorial' do
-    solver = Solver.new
 
     it 'Factorial test for positive integers' do
       num1 = 5
-      factorial_num = solver.factorial(num1)
+      factorial_num = Solver.new.factorial(num1)
       expect(factorial_num).to eq(120)
     end
 
     it 'Factorial test for zero' do
       num2 = 0
-      factorial_zero = solver.factorial(num2)
+      factorial_zero = Solver.new.factorial(num2)
       expect(factorial_zero).to eq(1)
+    end
+
+    it 'Factorial of negative numbers' do
+      expect {Solver.new.factorial(-7) }.to raise_error(StandardError)
     end
   end
 end
